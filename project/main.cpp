c092501e588f99437e09fba5277c48c66c1d0081
naaaaaaq <naaaaaaq@163.com>
2019-11-20 20:34:17 +0800

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by HFY on 2019/11/20.
+//  Copyright © 2019 nsblacker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
